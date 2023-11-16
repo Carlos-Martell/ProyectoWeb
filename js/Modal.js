1c1186dfabe3736793c759a4433f4146246be7ea
Carlos <i202336333@cibertec.edu.pe>
2023-11-16 13:33:26 -0500

    ProyectoWeb

---
+++ js/Modal.js
@@ -0,0 +1,22 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("openModal");
+var span = document.getElementById("closeModal");
+var spanFooter = document.getElementById("closeModalFooter");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+spanFooter.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
