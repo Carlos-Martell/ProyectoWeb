a0cbc334898e8334af506b27d29a60c628efc016
Carlos <i202336333@cibertec.edu.pe>
2023-11-17 22:09:00 -0500

    Cambio-FINAL

--- js/Modal.js
+++ js/Modal.js
@@ -2,17 +2,29 @@ var modal = document.getElementById("myModal");
 var btn = document.getElementById("openModal");
 var span = document.getElementById("closeModal");
 var spanFooter = document.getElementById("closeModalFooter");
+var submitButton = document.getElementById("submitForm");
 
+submitButton.onclick = function() {
+  location.reload();
+}
 btn.onclick = function() {
   modal.style.display = "block";
+  modal.classList.remove('close');
+  modal.classList.add('open');
+  overlay.style.display = "block";
 }
 
 span.onclick = function() {
-  modal.style.display = "none";
+ modal.classList.remove('open');
+  modal.classList.add('close');
+  overlay.style.display = "none";
+  setTimeout(function() { modal.style.display = "none"; }, 500);
 }
+
 
 spanFooter.onclick = function() {
   modal.style.display = "none";
+  overlay.style.display = "none";
 }
 
 window.onclick = function(event) {
@@ -20,3 +32,4 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
